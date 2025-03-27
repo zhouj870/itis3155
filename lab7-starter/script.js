04e655f82c63de9263aa81073ea4b8f9620889bf
Jie Zhou <Zhouj870@gmail.com>
2025-03-27 16:55:28 -0400

    css and javascript changes

--- lab7-starter/script.js
+++ lab7-starter/script.js
@@ -1,3 +1,15 @@
 
 'use strict';
 
+const accordionBtn = Array.from(document.querySelectorAll(".accordion-label"));
+accordionBtn.addEventListener('click',expand);
+const accordionSibling = document.querySelectorAll(".accordion-content");
+
+
+function expand(e){
+    const btn = event.target(accordionBtn);
+    console.log('button clicked');
+    btn.classList.toggle('open');
+
+}
+
