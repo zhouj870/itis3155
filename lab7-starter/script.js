8a192c45472d05ec25e04ef9919f36e0f7831eff
Jie Zhou <Zhouj870@gmail.com>
2025-03-29 16:21:43 -0400

    Added js for content and transistions

--- lab7-starter/script.js
+++ lab7-starter/script.js
@@ -1,15 +1,38 @@
 
 'use strict';
 
+const root = document.documentElement;
+
 const accordionBtn = Array.from(document.querySelectorAll(".accordion-label"));
-accordionBtn.addEventListener('click',expand);
-const accordionSibling = document.querySelectorAll(".accordion-content");
+function buttonClick(event)
+{
+    const btn = event.target;
+    const content = btn.nextElementSibling;
 
+    //  toggle open class for when button is clicked
+    btn.classList.toggle("open");
+    content.classList.toggle("open");
 
-function expand(e){
-    const btn = event.target(accordionBtn);
-    console.log('button clicked');
-    btn.classList.toggle('open');
+    // set height for css
+    root.style.setProperty("--content-height", content.scrollHeight + 'px');
 
+    //close other sections when clicked
+    for(let i = 0; i < accordionBtn.length; i++)
+    {
+        let button = accordionBtn[i];
+        let otherContent = button.nextElementSibling;
+        if(button !== btn && button.classList.contains("open"))
+        {
+            button.classList.remove("open");
+            otherContent.classList.remove("open");
+
+        }
+
+    }
 }
+
+for (let i = 0; i < accordionBtn.length; i++) {
+    accordionBtn[i].addEventListener("click", buttonClick);
+}
+
 
